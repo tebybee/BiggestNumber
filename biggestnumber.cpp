6bca21071eecbabba119b7dc107b4770a4ac3701
tebybee <tbybee@mail.csuchico.edu>
2018-02-06 11:32:43 -0800

    compares twoinputed numbers and returns the largest of the two.

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,16 +1,27 @@
-//Author:
+//Author:Travis Bybee
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
+int number1;
+int number2;
+int biggest;
   cout<<"Please enter a whole number:\n";
-  
+  cin>> number1; 
   cout<<"Please enter another whole number:\n";
-
+  cin>> number2;
+  if(number1 > number2)
+  { 
+    biggest= number1;
+  }
+  else
+  {
+    biggest= number2;
+  }
   cout<<"Of those two numbers, the biggest is: ";
+  cout<< biggest;
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
